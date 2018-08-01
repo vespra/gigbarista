6fbc3fac8cade78fcfe8d176d925e71966b61387
CORP\daniela.desira <daniela.desira@igamingcloud.com>
2018-08-01 15:51:54 +0200

    Added cookie package

--- gig-barista/src/state/index.js
+++ gig-barista/src/state/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import {createStore, combineReducers} from 'redux';
+import Cookies from 'universal-cookie';
 
 import allReducers from './reducers';
-
 import defaultSetup from '../data/default-setup';
 import coffeeTypes from '../data/coffee-types';
 
-//https://plainjs.com/javascript/utilities/set-cookie-get-cookie-and-delete-cookie-5/
-function getCookie(name) {
-    var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
-    return v ? v[2] : null;
-}
+const cookies = new Cookies();
 
 // create an object for the default data
 const defaultState = {
@@ -19,8 +15,8 @@ const defaultState = {
 }
 
 // if cookie exists, update values of default state
-defaultState.config.coffee_bean_type = getCookie('coffee_bean_type');
-defaultState.config.coffee_amount = getCookie('coffee_amount');
+defaultState.config.coffee_bean_type = cookies.get('coffee_bean_type');
+defaultState.config.coffee_amount = cookies.get('coffee_amount');
 
 export default function configureStore() {
   const store = createStore(allReducers, defaultState);
--- gig-barista/src/state/reducers/reducer-config.js
+++ gig-barista/src/state/reducers/reducer-config.js
@@ -1,9 +1,6 @@
-function setCookie(cname, cvalue, exdays) {
-    var date = new Date();
-    var midnight = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 23, 59, 59);
-    var expires = "expires="+ midnight;
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
+//import {setCookie} from '../../utils/utils';
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
 
 export default function config(state = [], action) {
   switch(action.type) {
@@ -12,8 +9,8 @@ export default function config(state = [], action) {
       //
       const coffee_bean_type = action.coffeeType;
       const coffee_amount = action.coffeeAmount;
-      setCookie('coffee_bean_type', coffee_bean_type, 1);
-      setCookie('coffee_amount', coffee_amount, 1);
+      cookies.set('coffee_bean_type', coffee_bean_type, 1);
+      cookies.set('coffee_amount', coffee_amount, 1);
       return {
         ...state,
         coffee_bean_type,
